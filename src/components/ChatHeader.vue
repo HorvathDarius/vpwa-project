35f68fecb0c1edde06d20a8e0030c4ec3e20e7bc
Darius Horvath <dariushorvath33@gmail.com>
2024-10-12 23:23:37 +0200

    feat: Update chat message space layout, start implementation of chat actions

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -1,35 +1,19 @@
 <template>
   <q-header elevated class="transparent see-through-style">
-    <q-toolbar class="text-black">
-      <q-btn round flat icon="keyboard_arrow_left" />
-
+    <q-toolbar class="text-black q-px-lg">
       <q-btn round flat>
         <q-avatar>
-          <img :src="currentConversation.avatar" />
+          <img src="/blankProfile.jpg" />
         </q-avatar>
       </q-btn>
 
       <span class="q-subtitle-1 q-pl-md text-white text-bold">
-        {{ currentConversation.person }}
+        SLACK VPWA PROJECT
       </span>
 
       <q-space />
-
-      <q-btn round flat icon="more_vert">
-        <q-menu auto-close :offset="[110, 0]">
-          <q-list style="min-width: 150px">
-            <q-item clickable>
-              <q-item-section></q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
     </q-toolbar>
   </q-header>
 </template>
 
-<script setup>
-const { currentConversation } = defineProps({
-  currentConversation: Object,
-});
-</script>
+<script setup></script>
--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -6,32 +6,29 @@
   >
     <ChatHeader :currentConversation="currentConversation" />
 
-    <ChatDrawer
-      :search="search"
-      :conversations="conversations"
-      :handleSetConversation="setCurrentConversation"
-    />
+    <q-page
+      class="transparent see-through-style fit row wrap justify-start items-start content-start"
+    >
+      <ChatDrawer
+        :conversations="conversations"
+        :handleSetConversation="setCurrentConversation"
+      />
 
-    <q-page-container class="fit transparent see-through-style">
-      <q-page>
-        <ChatMessageSpace />
-      </q-page>
-    </q-page-container>
+      <q-separator vertical inset style="width: 2px" />
 
-    <ChatFooter :message="message" />
+      <!-- <q-page-container class="transparent see-through-style"> -->
+      <ChatMessageSpace />
+    </q-page>
   </q-layout>
 </template>
 
 <script setup>
-import ChatFooter from 'src/components/ChatFooter.vue';
 import ChatHeader from 'src/components/ChatHeader.vue';
 import ChatDrawer from 'src/components/ChatDrawer.vue';
 import { ref, computed } from 'vue';
 import ChatMessageSpace from 'src/components/ChatMessageSpace.vue';
 import { conversations } from 'src/mocks/chatChannelMock';
 
-const search = ref('');
-const message = ref('');
 const currentConversationIndex = ref(0);
 
 const currentConversation = computed(() => {
