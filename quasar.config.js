bd86e92b1c12ca3074014e61b2dcf19afe010372
Martin Sivak <xsivakm1@stuba.sk>
2024-10-15 16:09:37 +0200

    config: setup sass

--- quasar.config.js
+++ quasar.config.js
@@ -39,6 +39,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      sassVariables: 'src/css/quasar.variables.sass',
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node20',
