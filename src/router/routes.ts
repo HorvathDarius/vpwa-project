cab51f32821a5de01ddfd5c84e79fc107dd02160
Darius Horvath <dariushorvath33@gmail.com>
2024-11-09 21:28:24 +0100

    feat: implement logic for auth with backend

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,12 +3,19 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    meta: { requiresAuth: true },
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/ChatPage.vue') },
+    ],
+  },
+  {
+    path: '/auth',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
       { path: 'login', component: () => import('pages/LoginPage.vue') },
       { path: 'register', component: () => import('pages/RegisterPage.vue') },
-    ],
+    ]
   },
 
   // Always leave this as last one,
